7e78dac674e37bf5908436f8d142a60ab25c8208
sadiebyun <sadiebyun@gmail.com>
2016-08-10 09:47:42 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function testJavaFunction() {
+		return 0;
+}
+
+function bigImg() {
+	var x = document.getElementById("pig");
+    x.style.height = "300px";
+}
+
+function normalImg() {
+	var x = document.getElementById("pig");
+    x.style.height = "250px";
+}
+
+function bigImg_1() {
+	var x = document.getElementById("dylan");
+    x.style.height = "300px";
+}
+
+function normalImg_1() {
+	var x = document.getElementById("dylan");
+    x.style.height = "250px";
+}
+
+function bigImg_2() {
+	var x = document.getElementById("ball");
+    x.style.height = "300px";
+}
+
+function normalImg_2() {
+	var x = document.getElementById("ball");
+    x.style.height = "250px";
+}
